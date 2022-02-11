fb7e69c65d625d3b039ea7bc8d61306d1b9dcd9b
ricophie03 <ricophie33@gmail.com>
2022-02-12 01:15:47 +0800

    menambahkan Bitcoin Tracker ke Github

---
+++ android/app/src/main/kotlin/com/enricophie/flutter_bitcoin_ticker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.enricophie.flutter_bitcoin_ticker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'price.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() {
+  HttpOverrides.global =
+      MyHttpOverrides(); // hapus object ini kalau app mau di release.
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
